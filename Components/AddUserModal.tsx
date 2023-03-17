f6ed4c2c275e28c33a36ca2928c3991b798591a1
Pallab6Sense <pallab@6sensehq.com>
2023-03-18 00:30:51 +0600

    Fixed the Modal Issue

--- Components/AddUserModal.tsx
+++ Components/AddUserModal.tsx
@@ -1,9 +1,10 @@
 import axios from 'axios';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import ReactDOM from 'react-dom';
 interface propsType {
   open: boolean;
   setmodal: any;
-};
+}
 export default function AddUserModal(props: propsType) {
   const { open, setmodal } = props;
   const [name, setName] = useState('');
@@ -22,65 +23,74 @@ export default function AddUserModal(props: propsType) {
         console.log(response.data);
       });
   };
-  if (!open) return null;
-  
-  return (
-    <>
-    <div className="container">
-      <div className="add-user-modal">
-        <div className="overlay">
-        <div className="form">
-          <form action="" onSubmit={handleSubmit}>
-            <div className="">
-              <label className="">Name</label>
-              <input
-                type="text"
-                value={name}
-                onChange={(e) => {
-                  setName(e.target.value);
-                }}
-                required
-              />
-            </div>
-            <div className="">
-              <label className="">Address</label>
-              <input
-                type="text"
-                value={address}
-                onChange={(e) => {
-                  setAddress(e.target.value);
-                }}
-                required
-              />
-            </div>
-            <div className="">
-              <label className="">Phone Number</label>
-              <input
-                type="text"
-                value={phone}
-                onChange={(e) => {
-                  setPhone(e.target.value);
-                }}
-                required
-              />
-            </div>
-            <div className="">
-              <button className="submit" type="submit">
-                Add
-              </button>
-            </div>
-            <div className="">
-              <button className="close" onClick={() => setmodal(!open)}>
-                Close
-              </button>
-            </div>
-          </form>
-        </div>
-        </div>
-        
+  const [isBrowser, setIsBrowser] = useState(false);
+  useEffect(() => {
+    setIsBrowser(true);
+  }, []);
 
+  const modalContent = open ? (
+    <div className="container">
+      <div className="overlay">
+      <div className="add-user-modal">
+        
+          <div className="form">
+            <form action="" onSubmit={handleSubmit}>
+              <div className="">
+                <label className="">Name</label>
+                <input
+                  type="text"
+                  value={name}
+                  onChange={(e) => {
+                    setName(e.target.value);
+                  }}
+                  required
+                />
+              </div>
+              <div className="">
+                <label className="">Address</label>
+                <input
+                  type="text"
+                  value={address}
+                  onChange={(e) => {
+                    setAddress(e.target.value);
+                  }}
+                  required
+                />
+              </div>
+              <div className="">
+                <label className="">Phone Number</label>
+                <input
+                  type="text"
+                  value={phone}
+                  onChange={(e) => {
+                    setPhone(e.target.value);
+                  }}
+                  required
+                />
+              </div>
+              <div className="">
+                <button className="submit" type="submit">
+                  Add
+                </button>
+              </div>
+              <div className="">
+                <button className="close" onClick={() => setmodal(!open)}>
+                  Close
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
       </div>
-      </div>
-    </>
-  );
+    </div>
+  ) : null;
+
+  if (isBrowser) {
+    return ReactDOM.createPortal(
+      modalContent,
+      document.getElementById('modal-root')
+    );
+  } else {
+    return null;
+  }
 }
